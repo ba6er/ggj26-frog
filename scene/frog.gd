bb2d42644fdf9a128352909c49a27ed32f6eab55
Ba6er <ba6er@tutamail.com>
2026-01-31 16:20:12 +0100

    basic frog jump

--- scene/frog.gd
+++ scene/frog.gd
@@ -1,19 +1,69 @@
 extends Node2D
+class_name Frog
 
+enum Direction {LEFT, RIGHT, UP, DOWN}
+
+@export var jump_duration: float = 0.3
+@export var on_maze: Vector2i = Vector2i(0, 0)
+@export var dir: Direction = Direction.DOWN
+
+var can_move := true
+var jump_transition : Array[Tween.TransitionType] = [
+	Tween.TRANS_CUBIC,
+	Tween.TRANS_CUBIC,
+	Tween.TRANS_BACK,
+	Tween.TRANS_BACK,
+]
+var jump_easings : Array[Tween.EaseType] = [
+	Tween.EASE_OUT,
+	Tween.EASE_OUT,
+	Tween.EASE_OUT,
+	Tween.EASE_IN,
+]
+
+@onready var sprite: Sprite2D = $sprite
 @onready var maze: LilyMaze = get_parent()
 
-# Called when the node enters the scene tree for the first time.
+func try_to_move(new_dir: Direction) -> void:
+	if new_dir == Direction.LEFT and on_maze.x > 0:
+		on_maze.x -= 1
+	if new_dir == Direction.RIGHT and on_maze.x < maze.size.x - 1:
+		on_maze.x += 1
+	if new_dir == Direction.UP and on_maze.y > 0:
+		on_maze.y -= 1
+	if new_dir == Direction.DOWN and on_maze.y < maze.size.y - 1:
+		on_maze.y += 1
+	dir = new_dir
+	sprite.frame = dir
+	
+	var new_pos := maze_pos_to_real_pos()
+	if new_pos == position:
+		return
+	
+	can_move = false
+	var tw := create_tween()
+	tw.set_trans(jump_transition[dir])
+	tw.set_ease(jump_easings[dir])
+	tw.tween_property(self, "position", new_pos, jump_duration)
+	await tw.finished
+	can_move = true
+
+func maze_pos_to_real_pos() -> Vector2:
+	return Vector2(maze.gap.x * on_maze.x, maze.gap.y * on_maze.y)
+
 func _ready() -> void:
-	pass # Replace with function body.
+	sprite.frame = dir
+	position = maze_pos_to_real_pos()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	if can_move == false:
+		return
+	
 	if Input.is_action_just_pressed("move_left"):
-		position.x -= maze.maze_gap_x
+		try_to_move(Direction.LEFT)
 	if Input.is_action_just_pressed("move_right"):
-		position.x += maze.maze_gap_x
+		try_to_move(Direction.RIGHT)
 	if Input.is_action_just_pressed("move_up"):
-		position.y -= maze.maze_gap_y
+		try_to_move(Direction.UP)
 	if Input.is_action_just_pressed("move_down"):
-		position.y += maze.maze_gap_y
+		try_to_move(Direction.DOWN)
--- scene/maze.gd
+++ scene/maze.gd
@@ -1,28 +1,25 @@
 extends Node2D
 class_name LilyMaze
 
-@export var maze_width: int = 7
-@export var maze_height: int = 7
-@export var maze_gap_x: int = 48
-@export var maze_gap_y: int = 48
+@export var size: Vector2i = Vector2i(7, 7)
+@export var gap: Vector2i = Vector2i(50, 50)
 
 const LILY_PREFAB := preload("res://scene/lilypad.tscn")
 var lily_pads: Array[Array]
 
 func _ready() -> void:
 	var lily_pos = Vector2(0, 0)
-	lily_pads.resize(maze_height)
-	for i in maze_height:
-		lily_pads[i].resize(maze_width)
-		for j in maze_width:
+	lily_pads.resize(size.y)
+	for i in size.y:
+		lily_pads[i].resize(size.x)
+		for j in size.x:
 			var lp = LILY_PREFAB.instantiate() as LilyPad
 			lp.position = lily_pos
 			lily_pads[i][j] = lp
 			add_child(lp)
-			lily_pos.x += maze_gap_x
-		lily_pos.y += maze_gap_y
+			lily_pos.x += gap.x
+		lily_pos.y += gap.y
 		lily_pos.x = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
